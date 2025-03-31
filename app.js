59f3ecf6ddc331de79acc92db2e1dc33b6b99d3a
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-03-30 23:54:43 -0300

    Create a function to generate the computer choice
    The function returns 'rock', 'paper' or 'scissors'

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function getComputerChoice() {
+	const randomChoice = Math.floor(Math.random() * 3);
+
+	switch (randomChoice) {
+		case 0:
+			return "rock";
+		case 1:
+			return "paper";
+		case 2:
+			return "scissors";
+	}
+}
+
+const computerChoice = getComputerChoice();
+
+console.log({ computerChoice });
