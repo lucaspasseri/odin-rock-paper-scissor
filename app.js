44b885fac3760964eeba4628a3996c495dd427d5
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-03-31 09:59:28 -0300

    Create a function that plays the game
    The game plays in a loop until someone have 5 victory points

--- app.js
+++ app.js
@@ -21,29 +21,45 @@ function getHumanChoice() {
 	return answer;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-	if (humanChoice === computerChoice) {
-		console.log("It is a draw!");
-	} else if (humanChoice === "rock" && computerChoice === "scissors") {
-		humanScore += 1;
-		console.log("You won!\nRock beats scissors.");
-	} else if (humanChoice === "scissors" && computerChoice === "paper") {
-		humanScore += 1;
-		console.log("You won!\nScissors beats paper.");
-	} else if (humanChoice === "paper" && computerChoice === "rock") {
-		humanScore += 1;
-		console.log("You won!\nPaper beats rock.");
+	let computerChoice;
+	let humanChoice;
+
+	function playRound(humanChoice, computerChoice) {
+		computerChoice = getComputerChoice();
+		humanChoice = getHumanChoice();
+
+		if (humanChoice === computerChoice) {
+			console.log("It is a draw!");
+		} else if (humanChoice === "rock" && computerChoice === "scissors") {
+			humanScore += 1;
+			console.log("You won!\nRock beats scissors.");
+		} else if (humanChoice === "scissors" && computerChoice === "paper") {
+			humanScore += 1;
+			console.log("You won!\nScissors beats paper.");
+		} else if (humanChoice === "paper" && computerChoice === "rock") {
+			humanScore += 1;
+			console.log("You won!\nPaper beats rock.");
+		} else {
+			computerScore += 1;
+			console.log(
+				`You lost!\n${capitalize(computerChoice)} beats ${humanChoice}.`
+			);
+		}
+	}
+
+	while (humanScore !== 5 && computerScore !== 5) {
+		playRound(humanChoice, computerChoice);
+	}
+
+	if (humanScore > computerScore) {
+		console.log(`Well done, you won!\n${humanScore} x ${computerScore}`);
 	} else {
-		computerScore += 1;
-		console.log(
-			`You lose!\n${capitalize(computerChoice)} beats ${humanChoice}.`
-		);
+		console.log(`You lost!\n${computerScore} x ${humanScore}`);
 	}
 }
 
-playRound(humanChoice, computerChoice);
+playGame();
